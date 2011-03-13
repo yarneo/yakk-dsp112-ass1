d53bc239b0f3ce25d906080e63b8a98862049408
kobykahane@gmail.com <kobykahane@gmail.com@edbe58a1-d017-ce75-fe18-91e8eb67f2b4>
2011-03-13 17:03:08 +0000

    Add common package and Consts class.

--- trunk/Assignment1/src/worker/Main.java
+++ trunk/Assignment1/src/worker/Main.java
@@ -7,6 +7,16 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		/*
+		 * get a PDF message from an SQS queue.
+		 * download the PDF file indicated in the message.
+		 * convert the first page of the PDF file into an image file.
+		 * upload the image file to S3.
+		 * put a message in an SQS queue indicating the original URL of the PDF and the S3 url of the new image file
+		 * remove the PDF message from the SQS queue.
+		 */
+		
 
 	}
 
