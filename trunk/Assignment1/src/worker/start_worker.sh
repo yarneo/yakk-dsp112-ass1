a365370c818078d9c899d84e29c3b94ae5528625
kobykahane@gmail.com <kobykahane@gmail.com@edbe58a1-d017-ce75-fe18-91e8eb67f2b4>
2011-03-27 14:07:49 +0000

    Correct SVN path in cloud startup scripts.

--- trunk/Assignment1/src/worker/start_worker.sh
+++ trunk/Assignment1/src/worker/start_worker.sh
@@ -4,7 +4,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y install subversion ant ivy default-jdk
 date >> /tmp/worker_node.txt
 cd ~ubuntu
-sudo -u ubuntu svn export http://yakk-dsp112-ass1.googlecode.com/svn/trunk/ assignment1
+sudo -u ubuntu svn export http://yakk-dsp112-ass1.googlecode.com/svn/trunk/Assignment1 assignment1
 cd assignment1
 echo accessKey=REPLACED_WITH_ACCESS_KEY >> src/AwsCredentials.properties
 echo secretKey=REPLACED_WITH_SECRET_KEY >> src/AwsCredentials.properties
