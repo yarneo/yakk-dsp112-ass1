e0203f7799f20144e6542d7382d4d79131e31224
kobykahane@gmail.com <kobykahane@gmail.com@edbe58a1-d017-ce75-fe18-91e8eb67f2b4>
2011-03-27 12:59:02 +0000

    Change S3 bucket name to lowercase.

--- trunk/Assignment1/src/common/Consts.java
+++ trunk/Assignment1/src/common/Consts.java
@@ -3,7 +3,7 @@ package common;
 public class Consts {
 	public static String WORKER_PDF_REQUEST_QUEUE_NAME = "PdfRequestQueue";
 	public static String WORKER_PDF_RESPONSE_QUEUE_NAME = "PdfResponseQueue";
-	public static String WORKER_THUMBNAIL_BUCKET_NAME = "PdfThumbnailBucket-bbbd3ade-ae19-4788-987b-7fbbe9f8a959";
+	public static String WORKER_THUMBNAIL_BUCKET_NAME = "pdfthumbnailbucket-bbbd3ade-ae19-4788-987b-7fbbe9f8a959";
 	public static String LINKS_BUCKET_NAME = "pdfsassignment1-bbbd3ade-ae19-4788-987b-7fbbe9f8a959";
 	public static String INPUT_QUEUE_NAME = "queueIn";
 	public static String OUTPUT_QUEUE_NAME = "queueOut";
