eb2306666d6b915296d5d5512c25c0d3232e56d5
kobykahane@gmail.com <kobykahane@gmail.com@edbe58a1-d017-ce75-fe18-91e8eb67f2b4>
2011-07-20 15:57:40 +0000

    Set to 10 iterations and non-uniform initial distribution.

--- Assignment3/src/MapReduce/ContextInfoMapper.java
+++ Assignment3/src/MapReduce/ContextInfoMapper.java
@@ -1,12 +1,4 @@
-
-/*
- * ContextInfoMapper.java
- *
- * Created on Jun 6, 2011, 4:47:42 PM
- */
-
 package MapReduce;
-
 
 import java.io.IOException;
 // import org.apache.commons.logging.Log;
@@ -16,33 +8,25 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 
-/**
- *
- * @author yarneo
- */
 public class ContextInfoMapper extends Mapper<Text,LongWritable,Text,Pair> {
-	// The Karmasphere Studio Workflow Log displays logging from Apache Commons Logging, for example:
-	// private static final Log LOG = LogFactory.getLog("MapReduce.ContextInfoMapper");
-
 	@Override
 	protected void map(Text key, LongWritable value, Context context) throws IOException, InterruptedException {
-		String[] strArr = key.toString().split("\\s+");
-		for(int i=0;i<strArr.length;i++) {
-			String outStr = "";
+		String[] ngramWords = key.toString().split("\\s+");
+		for(int i = 0; i < ngramWords.length; i++) {
+			String outContext = "";
 			String outWord = "";
-			for(int j=0;j<strArr.length;j++) {
-				if(i==j) {
-					outStr += "_____";
-					outWord = strArr[j];
+			for(int j = 0; j < ngramWords.length; j++) {
+				if(i == j) {
+					outContext += "_____";
+					outWord = ngramWords[j];
+				} else {
+					outContext += ngramWords[j];
 				}
-				else {
-					outStr += strArr[j];
-				}
-				if(j != (strArr.length-1)) {
-					outStr += " ";
+				if(j != (ngramWords.length - 1)) {
+					outContext += " ";
 				}
 			}
-			context.write(new Text(outStr), new Pair(new Text(outWord),value));
+			context.write(new Text(outContext), new Pair(new Text(outWord),value));
 		}
 	}
 }
